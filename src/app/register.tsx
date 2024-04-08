e4c40a9b9c6682f07533d5ddf28ad1e5dd8ec6a8
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-04-08 20:06:19 +0100

    register added

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -7,6 +7,7 @@ import axios from "axios"
 import { colors } from "@/styles/colors";
 
 import { api } from "@/server/api";
+import { useBadgeStore } from "@/store/badge-store"
 
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
@@ -19,12 +20,49 @@ export default function Register() {
   const [ email, setEmail] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
+  const badgeStore = useBadgeStore()
+
+  async function handleRegister() {
+    try{
+      if(!name.trim() || email.trim()){
+        return Alert.alert("Inscrição", "Preencha todos os campos!")
+      }
+      setIsLoading(true)
+
+      const registerResponse = await api.post(`/events/${EVENT_ID}/attendees`, {
+        name,
+        email,
+      })
+      if(registerResponse.data.attendeeId){
+        const badgeResponse = await api.get(  `/attendees/${registerResponse.data.attendeeId}/badge`)
+        badgeStore.save(badgeResponse.data.badge)
+
+        Alert.alert("Inscrição", "Inscrição realizada com sucesso!", [
+          { text: "OK", onPress: () => router.push("/ticket") }
+        ])
+      }
+
+    }catch(error){
+      console.log(error)
+      setIsLoading(false)
+
+      if(axios.isAxiosError(error)){
+        if(String(error.response?.data.message).includes("already registered"))
+        return Alert.alert("Inscrição", "Este e-mail já está cadastrado!")
+      }
+      Alert.alert("Inscrição", "Não foi possível fazer a inscrição")
+    }
+  }
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
-      <Image 
-        source={require("@/assets/")}
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
       />
+
       <View className="w-full mt-12 gap-3">
         <Input>
           <FontAwesome6
@@ -34,25 +72,28 @@ export default function Register() {
           />
           <Input.Field placeholder="Nome completo" onChangeText={setName} />
         </Input>
-        <Input >
-           <MaterialIcons
+
+        <Input>
+          <MaterialIcons
             name="alternate-email"
             color={colors.green[200]}
             size={20}
-           />
-           <Input.Field
+          />
+          <Input.Field
             placeholder="E-mail"
             keyboardType="email-address"
             onChangeText={setEmail}
-           />
+          />
         </Input>
+
         <Button
-         title="Realizar inscrição"
-         isLoading={isLoading}
+          title="Realizar inscrição"
+          isLoading={isLoading}
         />
+
         <Link
-        href="/"
-        className="text-gray-100 text-base font-bold text-center mt-8"
+          href="/"
+          className="text-gray-100 text-base font-bold text-center mt-8"
         >
           Já possui ingresso?
         </Link>
